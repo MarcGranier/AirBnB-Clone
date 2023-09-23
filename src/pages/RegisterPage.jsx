62afe05db64edf70d9d20e11e67324e775dcb1d2
Marc Granier <mgranier@comcast.net>
2023-09-23 17:01:35 -0400

    Tweaks to Register & Login Page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,7 +4,10 @@ export default function RegisterPage() {
 	return (
 		<div className='mt-4 grow flex items-center justify-around'>
 			<div className='mb-64'>
-				<h1 className='text-4xl text-center mb-4'>Register</h1>
+				<h1 className='text-l font-bold text-center mb-20'>
+					Log in or sign up
+				</h1>
+				<h2 className='text-2xl font-bold'>Welcome to Airbnb</h2>
 				<form className='max-w-md mx-auto'>
 					<input type='text' placeholder='John Doe' />
 					<input type='email' placeholder='Your email' />
